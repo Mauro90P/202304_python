664c7f08783044f94f7b7ad5cb0c83dc2074ec59
Mauricio Pardo <mauriciopardo.figueroa@gmail.com>
2023-05-27 18:49:55 -0400

    Ejercicios de FLASK

--- flask/fundamentals/hola_flask/server.py
+++ flask/fundamentals/hola_flask/server.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def hola_mundo():
     return 'Hola Mundo!'           # Devuelve la cadena '¡Hola Mundo!' como respuesta
 
-@app.reoute('/hello/<string:banana>/<int:num>')
+""" @app.reoute('/hello/<string:banana>/<int:num>')
 def hello(banana, num):
     return f"Hello{banana * num}"
 @app.route('/users/<username>/<id>') # para una ruta '/users/____/____', dos parámetros en la url se pasan como nombre de usuario e id
@@ -29,3 +29,4 @@ def hola(name):
 if __name__ == "__main__":        # Asegúrate de que este archivo se esté ejecutando directamente y no desde un módulo diferente
     # Ejecuta la aplicación en modo de depuración
     app.run(debug=True)
+ """
