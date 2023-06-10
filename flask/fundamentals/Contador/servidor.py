7585ffc6e157e9973ff8a13b762c851ad5e32376
Mauricio Pardo <mauriciopardo.figueroa@gmail.com>
2023-06-10 18:16:40 -0400

    Ejercicio de conunt

---
+++ flask/fundamentals/Contador/servidor.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+contador = 1
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', contador=contador)
+
+
+@app.route('/sumar')
+def sumar():
+    global contador
+    contador += 1
+    return str(contador)
+
+
+@app.route('/reset')
+def reset():
+    global contador
+    contador = 0
+    return str(contador)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
